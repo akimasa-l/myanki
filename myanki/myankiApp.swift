32d56bba8a22384a590f43304476ab95cb5d1e71
akimasa-l <liumingzheng8@outlook.com>
2024-06-10 21:36:11 +0900

    Initial Commit

---
+++ myanki/myankiApp.swift
@@ -0,0 +1,17 @@
+//
+//  myankiApp.swift
+//  myanki
+//
+//  Created by 劉明正 on 2024/06/10.
+//
+
+import SwiftUI
+
+@main
+struct myankiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
